7756973d7fa9d026aa48771c4a1ebf4869f8ba5c
codedhola <codedhola2000@gmail.com>
2023-07-25 19:50:49 +0100

    feat: Added delete functionality

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -7,12 +7,34 @@ const BlogDetails = () => {
   const { data, isLoading, isError } = useFetch(
     `http://localhost:8000/blogs/${id}`
   );
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log("An error occured while deleting");
+      });
+  };
+
   return (
     <div>
       <h1>BlogDetails</h1>
       {isLoading && <h1>Loading...</h1>}
       {isError && <h1>Invalid Parameter provided</h1>}
-      {data && <p>{data.body}</p>}
+      {data && (
+        <div>
+          <h1>{data.title}</h1>
+          <p>{data.body}</p>
+          <code>{data.author}</code>
+          <button onClick={handleDelete}>delete</button>
+        </div>
+      )}
     </div>
   );
 };
